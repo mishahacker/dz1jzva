80bbd036aceb141a32125a217d5e324687e28cc0
mishahacker <dj3c@yandex.ru>
2022-03-02 08:30:16 +0300

    Update dz2.java
    dz2

--- dz2.java
+++ dz2.java
@@ -1,49 +1,51 @@
-package ru.geekbrains.Java Corels1;
+//        1. Напишите метод, на вход которого подается двумерный строковый массив размером 4х4, при подаче массива другого размера необходимо
+//        бросить исключение MyArraySizeException.
+//        2. Далее метод должен пройтись по всем элементам массива, преобразовать в int, и просуммировать. Если в каком-то элементе массива
+//        преобразование не удалось (например, в ячейке лежит символ или текст вместо числа), должно быть брошено исключение MyArrayDataException –
+//        с детализацией, в какой именно ячейке лежат неверные данные.
+//        3. В методе main() вызвать полученный метод, обработать возможные исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.
 
-/*
-2. Добавить класс Team, который будет содержать название команды,массив из четырех участников (в конструкторе можно сразу указыватьвсех участников )
-*/
-public class Team {
-    private String name;// название команды
-    private Participant participants[] ;    // участники
+public class Main {
 
-    public Team(String name) {
-        this.name = name;
-    }
+    public static void main(String[] args) {
 
-    public Team(String name,Participant ... participantsGiven ) {
-        this.name = name;
-        this.participants = participantsGiven;
-    }
-  //метод вывода информации обо всех членах команды.
-    public void getTeamInfo() {
-        System.out.println("Команда: " + this.name );
-        for (Participant participant : participants) {
-            if (participant instanceof Dog) {
-                System.out.println("Собака " + participant.getName());
-            }
-            if (participant instanceof Cat) {
-                System.out.println("Кот " + participant.getName());
-            }
-            if (participant instanceof Robot) {
-                System.out.println("Робот " + participant.getName());
+        String[][] arr = new String[][]{{"1", "2", "3", "4"}, {"2", "2", "2", "3"}, {"1", "2", "2", "2"}, {"2", "2", "2", "2"}};
+        try {
+            try {
+                int result = method(arr);
+                System.out.println(result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Размер массива превышен!");
             }
         }
+        catch (MyArrayDataException e) {
+            System.out.println("Неправильное значение массива!");
+            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+        }
+
     }
-//
-    public void showResults(){
-        for (Participant participant : participants) {
-                if (!participant.isOnDistance()) {
-                    break;
+
+
+    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
                 }
             }
+
         }
-    public void doIt(Obstacle obstacle){
-        for (Participant participant : participants) {
-            obstacle.doIt(participant);
-        }
+        return count;
     }
-
-
 
 }
